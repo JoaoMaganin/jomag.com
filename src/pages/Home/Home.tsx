d8941c23e4e05b07d6c6a27d79a0962aaf3a45f0
JoaoMaganin <joaovitormontemor138@gmail.com>
2025-10-07 15:53:17 -0300

    refactor: changing site layout

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import Experience from "../../components/Experience/Experience";
 import Navbar from "../../components/Navbar/Navbar";
 import ProjectSlider from "../../components/ProjectSlider/ProjectSlider";
 import WordRotator from "../../components/WordRotator/WordRotator";
@@ -8,23 +9,29 @@ const Home = () => {
     return (
         <div>
             <Navbar />
-            <section className="main-section">
-                <div className="word-rotator-section">
-                    <WordRotator />
-                </div>
-                <div className="project-slider-section">
-                    <ProjectSlider />
-                </div>
-            </section>
+            <div className="center">
+                <section className="main-section">
+                    <div className="word-rotator-section">
+                        <WordRotator />
+                    </div>
+                    <div className="project-slider-section">
+                        <ProjectSlider />
+                    </div>
+                </section>
 
-            <section className="about-me">
-                <div>
-                    <p>Muito prazer! Me chamo João Vitor Maganin, sou um desenvolvedor fullstack brasileiro,
-                    formado em Ciência da Computação e possuo experiência profissional como analista de sistemas.
-                    Sou apaixonado por novas tecnologias e por resolver problemas, sou movido a desafios.
-                    </p>
-                </div>
-            </section>
+                <section className="about-me">
+                    <div>
+                        <p>Muito prazer! Me chamo João Vitor Maganin, sou um desenvolvedor fullstack brasileiro,
+                            formado em Ciência da Computação e possuo experiência profissional como analista de sistemas.
+                            Sou apaixonado por novas tecnologias e por resolver problemas, sou movido a desafios.
+                        </p>
+                    </div>
+                </section>
+
+                <section className="my-experiences">
+
+                </section>
+            </div>
         </div>
     )
 }
